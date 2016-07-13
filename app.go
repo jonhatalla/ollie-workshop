849cf13f2e03cfd499bee7fbe815e3b6875fe148
bhenderson <bhenderson@yp.com>
2016-07-13 12:07:58 -0700

    Rename leap to app.

--- app.go
+++ app.go
@@ -1,6 +1,27 @@
 package main
 
+import (
+	"os"
+
+	"github.com/hybridgroup/gobot"
+	"github.com/hybridgroup/gobot/platforms/ble"
+)
+
+var ollie *ble.SpheroOllieDriver
 
 func main() {
-  leapMain()
+	gbot := gobot.NewGobot()
+
+	bleAdaptor := ble.NewBLEClientAdaptor("ble", os.Args[1])
+	ollie = ble.NewSpheroOllieDriver(bleAdaptor, "ollie")
+
+	robot := gobot.NewRobot("ollieBot",
+		[]gobot.Connection{bleAdaptor},
+		[]gobot.Device{ollie},
+		listenToKeyboard,
+	)
+
+	gbot.AddRobot(robot)
+
+	gbot.Start()
 }
